fd4643a17d0edd1b1bbfb58873ed320fdd52d93f
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-07 10:08:50 +0100

    Decode now checks for correct mimetype

--- decode.go
+++ decode.go
@@ -14,10 +14,13 @@ func Decode(r io.ReaderAt, size int64) (image.Image, error) {
 		return nil, err
 	}
 	required := 0
-	var merged *zip.File
+	var merged, mimetype *zip.File
 	for _, f := range zr.File {
 		switch f.Name {
-		case "mimetype", "stack.xml", "data", "Thumbnails/thumbnail.png":
+		case "stack.xml", "data", "Thumbnails/thumbnail.png":
+			required++
+		case "mimetype":
+			mimetype = f
 			required++
 		case "mergedimage.png":
 			merged = f
@@ -27,9 +30,26 @@ func Decode(r io.ReaderAt, size int64) (image.Image, error) {
 	if required < 5 {
 		return nil, ErrMissingRequired
 	}
+	if mimetype.UncompressedSize64 != len(mimetypeStr) {
+		return nil, ErrInvalidMimeType
+	} else {
+		mr, err := mimetype.Open()
+		if err != nil {
+			return nil, err
+		}
+		var mime [16]byte
+		_, err = io.ReadFull(mr, mime[:])
+		mr.Close()
+		if err != nil {
+			return nil, err
+		}
+		if string(mime) != mimetypeStr {
+			return nil, ErrInvalidMimeType
+		}
+	}
 	f, err := merged.Open()
 	if err != nil {
-		return
+		return nil, err
 	}
 	defer f.Close()
 	return png.Decode(f)
@@ -38,4 +58,5 @@ func Decode(r io.ReaderAt, size int64) (image.Image, error) {
 // Errors
 var (
 	ErrMissingRequired = errors.New("missing required file")
+	ErrInvalidMimeType = errors.New("invalid mime type")
 )
