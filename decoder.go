bd118208fc8a4e8a8845b9e5288d7a30ca3cbfad
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-10 15:49:03 +0100

    Added start of decoder

---
+++ decoder.go
@@ -0,0 +1,89 @@
+package xcf
+
+import (
+	"errors"
+	"image/color"
+	"io"
+)
+
+type Decoder struct {
+	r reader
+	s io.Seeker
+}
+
+func NewDecoder(r io.ReadSeeker) *Decoder {
+	return &Decoder{r: newReader(r), s: r}
+}
+
+type baseType uint8
+
+const (
+	BaseRGB       baseType = 0
+	BaseGrayScale baseType = 1
+	BaseIndexed   baseType = 2
+)
+
+type Image struct {
+	Width, Height uint32
+	BaseType      baseType
+	Colours       []color.Color
+}
+
+func (d *Decoder) Decode() (*Image, error) {
+	var header [14]byte
+	d.r.Read(header[:])
+	if d.r.Err != nil {
+		return nil, d.r.Err
+	}
+	if string(header[:9]) != "gimp xcf" || header[13] != 0 {
+		return nil, ErrInvalidHeader
+	}
+	var version uint8
+	switch string(header[8:12]) {
+	case "file":
+		version = 0
+	case "v001":
+		version = 1
+	case "v002":
+		version = 2
+	case "v003":
+		version = 3
+	default:
+		return nil, ErrUnsupportedVersion
+	}
+	var i Image
+	i.Width = d.r.ReadUint32()
+	i.Height = d.r.ReadUint32()
+	i.BaseType = baseType(d.r.ReadUint32())
+	if i.BaseType > BaseIndexed {
+		return nil, ErrInvalidBaseType
+	}
+	// read image properties
+	d.readImageProperties(&i)
+	// read layer pointers
+	layers := make([]uint32, 0, 32)
+	for {
+		pointer := d.r.ReadUint32()
+		if pointer == 0 {
+			break
+		}
+		layers = append(layers, pointer)
+	}
+	// read channel pointers
+	channels := make([]uint32, 0, 32)
+	for {
+		pointer := d.r.ReadUint32()
+		if pointer == 0 {
+			break
+		}
+		channels = append(channels, pointer)
+	}
+	return &i, nil
+}
+
+// Errors
+var (
+	ErrInvalidHeader      = errors.New("invalid xcf header")
+	ErrUnsupportedVersion = errors.New("unsupported version")
+	ErrInvalidBaseType    = errors.New("invalid basetype")
+)
