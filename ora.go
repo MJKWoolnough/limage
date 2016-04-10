f393dfaa86210a4d6c3aa4556819d18a5fb28df0
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-10 11:53:34 +0100

    Added start of stack decode

--- ora.go
+++ ora.go
@@ -1,6 +1,8 @@
 package ora
 
 import (
+	"archive/zip"
+	"encoding/xml"
 	"image/color"
 	"io"
 )
@@ -62,6 +64,41 @@ type Text struct {
 }
 
 func DecodeStack(r io.ReaderAt, size int64) (*Image, error) {
+	zr, err := zip.NewReader(r, size)
+	if err != nil {
+		return nil, err
+	}
+	required := 0
+	files := make(map[string]*zip.File)
+	var stack imageContent
+	for _, f := range zr.File {
+		switch f.Name {
+		case "stack.xml":
+			ms, err := f.Open()
+			if err != nil {
+				return nil, err
+			}
+			err = xml.NewDecoder(ms).Decode(&stack)
+			ms.Close()
+			if err != nil {
+				return nil, err
+			}
+			required++
+		case "mimetype":
+			if !checkMime(f) {
+				return nil, ErrInvalidMimeType
+			}
+			required++
+		case "data", "Thumbnails/thumbnail.png", "mergedimage.png":
+			required++
+		default:
+			files[f.Name] = f
+		}
+	}
+	if required < 5 {
+		return nil, ErrMissingRequired
+	}
+
 	return nil, nil
 }
 
