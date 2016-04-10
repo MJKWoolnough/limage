959955789bdc08f5c99e359fb001e04f9e7b4eba
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-10 15:48:46 +0100

    Added a wrapper around BigEndianWriter to add a WriteString method

---
+++ writer.go
@@ -0,0 +1,24 @@
+package xcf
+
+import (
+	"io"
+
+	"github.com/MJKWoolnough/byteio"
+)
+
+type writer struct {
+	byteio.StickyWriter
+}
+
+func newWriter(w io.Writer) *writer {
+	var write writer
+	write.Writer = byteio.BigEndianWriter{w}
+	return &write
+}
+
+func (w writer) WriteString(s string) {
+	b := make([]byte, len(s)+1)
+	copy(b, s)
+	w.WriteUint32(uint32(len(s)))
+	w.Write(b)
+}
