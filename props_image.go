7ee34f2ac9e3f523ab4c53571a50a0f4091deac8
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-13 16:54:32 +0100

    Added image props

---
+++ props_image.go
@@ -0,0 +1,323 @@
+package xcf
+
+import (
+	"errors"
+	"image/color"
+)
+
+func (d *Decoder) readColorMap() color.Palette {
+	num := d.r.ReadUint32()
+	c := make(color.Palette, num)
+	for i := uint32(0); i < num; i++ {
+		c[i] = color.RGBA{
+			d.r.ReadUint8(),
+			d.r.ReadUint8(),
+			d.r.ReadUint8(),
+			255,
+		}
+	}
+	return c
+}
+
+func (d *Decoder) readCompression() compression {
+	return compression(d.r.ReadUint8())
+}
+
+type orientation byte
+
+func (o orientation) IsHorizontal() bool {
+	return !o
+}
+
+func (o orientation) IsVertical() bool {
+	return o
+}
+
+func (o orientation) String() string {
+	if o {
+		return "horizontal"
+	}
+	return "vertical"
+}
+
+type guide struct {
+	coord       int32
+	orientation orientation
+}
+
+func (d *Decoder) readGuides(size uint32) []guide {
+	num := size / 5
+	g := make([]guide, num)
+	for i := uint32(0); i < num; i++ {
+		coord := d.r.ReadInt32()
+		o := d.r.ReadUint8()
+		if o != 1 && o != 2 {
+			if d.r.Err != nil {
+				d.r.Err = ErrInvalidState
+			}
+			return g
+		}
+		g[num] = guide{
+			coord:       coord,
+			orientation: orientation(o == 2),
+		}
+	}
+	return g
+}
+
+type resolution struct {
+	hres, vres float32
+}
+
+func (d *Decoder) readResolution() resolution {
+	return resolution{
+		d.r.ReadFloat32(),
+		d.r.ReadFloat32(),
+	}
+}
+
+type unit uint8
+
+const (
+	unitInches unit = 1
+	unitMM     unit = 2
+	unitPoints unit = 3
+	unitPicas  unit = 4
+)
+
+func (d *Decoder) readUnit() unit {
+	u := unit(d.r.ReadUint32())
+	switch u {
+	case unitInches, unitMM, unitPoints, unitPicas:
+		return u
+	}
+	if d.r.Err == nil {
+		d.r.Err = ErrInvalidState
+	}
+	return 0
+}
+
+type point struct {
+	anchorControl bool
+	x, y          float64
+}
+
+type path struct {
+	name   string
+	linked bool
+	closed bool
+	tattoo uint32
+	points []point
+}
+
+type paths struct {
+	index uint32
+	paths []path
+}
+
+func (d *Decoder) readPaths() paths {
+	index := d.r.ReadUint32()
+	num := d.r.ReadUint32()
+	p := paths{
+		index,
+		make([]path, num),
+	}
+	for i := uint32(0); i < num; i++ {
+		pt := &p.paths[i]
+		pt.name = d.r.ReadString()
+		linked := d.r.ReadUint32()
+		switch linked {
+		case 0:
+			pt.linked = false
+		case 1:
+			pt.linked = true
+		default:
+			d.r.Err = ErrInvalidState
+			return p
+		}
+		state := d.r.ReadUint8()
+		switch d.r.ReadUint32() {
+		case 0:
+			pt.closed = false
+		case 1:
+			pt.closed = true
+		default:
+			d.r.Err = ErrInvalidState
+			return p
+		}
+		np := d.r.ReadUint32()
+		version := d.r.ReadUint32()
+		if version == 2 || version == 3 {
+			d.r.ReadUint32()
+			if version == 3 {
+				pt.tattoo = d.r.ReadUint32()
+			}
+		} else if version != 1 {
+			if d.r.Err != nil {
+				return ErrInvalidState
+			}
+			return p
+		}
+		pt.points = make([]point, np)
+		for j := uint32(0); j < np; j++ {
+			switch d.r.ReadInt32() {
+			case 0:
+				pt.points[j].anchorControl = false
+			case 1:
+				pt.points[j].anchorControl = true
+			default:
+				d.r.Err = ErrInvalidState
+				return p
+			}
+			if version == 1 {
+				pt.points[j].x = float64(d.r.ReadInt32())
+				pt.points[j].y = float64(d.r.ReadInt32())
+			} else {
+				pt.points[j].x = float64(d.r.ReadFloat32())
+				pt.points[j].y = float64(d.r.ReadFloat32())
+			}
+		}
+	}
+	return p
+}
+
+type userUnit struct {
+	factor                           float32
+	digits                           uint32
+	id, symbol, abbrev, sname, pname string
+}
+
+func (d *Decoder) readUserUnit() userUnit {
+	var u userUnit
+	u.factor = d.r.ReadFloat32()
+	u.digits = d.r.ReadUint32()
+	u.id = d.r.ReadString()
+	u.symbol = d.r.ReadString()
+	u.abbrev = d.r.ReadString()
+	u.sname = d.r.ReadString()
+	u.pname = d.r.ReadString()
+	return u
+}
+
+type vectorpoint struct {
+	anchorControl                       bool
+	x, y, pressure, xtilt, ytilt, wheel float32
+}
+
+type stroke struct {
+	closed bool
+	points []vectorpoint
+}
+
+type vectorpath struct {
+	name            string
+	tattoo          uint32
+	visible, linked bool
+	parasites       []parasite
+	strokes         []stroke
+}
+
+type vectors struct {
+	index uint32
+	paths []vectorpath
+}
+
+func (d *Decoder) readVectors() vectors {
+	var v vectors
+	if d.r.ReadUint32() != 1 {
+		if d.r.Err != nil {
+			d.r.Err = ErrInvalidState
+		}
+		return v
+	}
+	v.index = d.r.ReadUint32()
+	n := d.r.ReadUint32()
+	v.paths = make([]vectorpath, n)
+	for i := uint32(0); i < i; i++ {
+		vp := &v.paths[i]
+		vp.name = d.r.ReadString()
+		vp.tattoo = d.r.ReadUint32()
+		switch d.r.ReadUint32() {
+		case 0:
+			vp.visible = false
+		case 1:
+			vp.visible = true
+		default:
+			d.r.Err = ErrInvalidState
+			return v
+		}
+		switch d.r.ReadUint32() {
+		case 0:
+			vp.linked = false
+		case 1:
+			vp.linked = true
+		default:
+			d.r.Err = ErrInvalidState
+			return v
+		}
+		m := d.r.ReadUint32()
+		k := d.r.ReadUint32()
+		vp.parasites = make([]parasite, m)
+		for j := uint32(0); j < m; j++ {
+			vp.parasites[j] = d.readParasites()
+		}
+		vp.strokes = make([]stroke, k)
+		for j := uint32(0); j < l; j++ {
+			if d.r.ReadUint32() != 1 { // should be 1, bezier curve
+				if d.r.Err != nil {
+					d.r.Err = ErrInvalidState
+				}
+				return v
+			}
+			switch d.r.ReadUint32() {
+			case 0:
+				vp.strokes[j].closed = false
+			case 1:
+				vp.strokes[j].closed = true
+			default:
+				d.r.Err = ErrInvalidState
+				return v
+			}
+			nf := d.r.ReadUint32()
+			if nf < 2 || nf > 6 {
+				d.r.Err = ErrInvalidState
+				return v
+			}
+			np := d.r.ReadUint32()
+			vp.strokes[j].points = make([]vectorpoint, np)
+			for k := uint32(0); k < np; k++ {
+				p := &vp.strokes[h].points[k]
+				switch d.r.ReadUint32() {
+				case 0:
+					p.anchorControl = false
+				case 1:
+					p.anchorControl = true
+				default:
+					d.r.Err = ErrInvalidState
+					return v
+				}
+				p.x = d.r.ReadFloat32()
+				p.y = d.r.ReadFloat32()
+				if nf > 3 {
+					p.pressure = d.r.ReadFloat32()
+					if nf > 4 {
+						p.xtilt = d.r.ReadFloat32()
+						if nf > 5 {
+							p.ytilt = d.r.ReadFloat32()
+							if nf == 6 {
+								p.wheel = d.r.ReadFloat32()
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return v
+}
+
+// Errors
+
+var (
+	ErrInvalidState = errors.New("invalid state")
+)
