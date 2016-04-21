f4c6800350b3977bd4b3eb9c58b20d0dc4007b21
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-21 15:19:36 +0100

    merged readLayerProperties into readLayer

--- decoder.go
+++ decoder.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 	"image/draw"
 	"io"
+	"os"
 )
 
 type props struct {
@@ -100,14 +101,85 @@ func (d *Decoder) Decode() (*Image, error) {
 	return i, nil
 }
 
-type layer struct{}
+type layer struct {
+	offsetX, offsetY                    int32
+	width, height                       uint32
+	name                                string
+	alpha                               bool
+	editMask, showMask, visible, locked bool
+}
 
 func (d *Decoder) readLayer() layer {
-	width := d.r.ReadUint32()
-	height := d.r.ReadUint32()
+	var l layer
+	l.width = d.r.ReadUint32()
+	l.height = d.r.ReadUint32()
 	typ := d.r.ReadUint32()
-	name := d.r.ReadString()
-	d.readLayerProperties()
+	l.name = d.r.ReadString()
+
+Props:
+	for {
+		propID := property(d.r.ReadUint32())
+		propLength := d.r.ReadUint32()
+		switch propID {
+		case propEnd:
+			break Props
+		case propActiveLayer:
+			// a := d.readActiveLayer()
+			// no data, just set as active layer
+		case propFloatingSelection:
+			f := d.r.ReadUint32()
+			_ = f
+		case propOpacity:
+			o := d.readOpacity()
+			_ = o
+		case propApplyMask:
+			a := d.readBool()
+			_ = a
+		case propEditMask:
+			l.editMask = d.readBool()
+			_ = e
+		case propMode:
+			m := d.readMode()
+			_ = m
+		case propLinked:
+			l := d.readBool()
+			_ = l
+		case propLockAlpha:
+			l := d.readBool()
+			_ = l
+		case propOffsets:
+			l.offsetX = d.r.ReadInt32()
+			l.offsetY = d.r.ReadInt32()
+		case propShowMask:
+			l.showMask = d.readBool()
+			_ = s
+		case propTattoo:
+			t := d.readTattoo()
+			_ = t
+		case propParasites:
+			p := d.readParasites(propLength)
+			_ = p
+		case propTextLayerFlags:
+			t := d.readTextLayerFlags()
+			_ = t
+		case propLockContent:
+			l.locked = d.readBool()
+		case propVisible:
+			l.visible = d.readBool()
+		case propGroupItem:
+			// g := d.readGroupItem()
+			// no data, just set as item group
+		case propItemPath:
+			i := d.readItemPath(propLength)
+			_ = i
+		case propGroupItemFlags:
+			g := d.r.ReadUint32()
+			_ = g
+		default:
+			d.s.Seek(int64(propLength), os.SEEK_CUR)
+		}
+	}
+
 	hptr := d.r.ReadUint32()
 	mptr := d.r.ReadUint32()
 	switch typ {
