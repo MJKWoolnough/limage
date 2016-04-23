693d6a097e689cfc0f2cef5fb4e3e378b7f42548
Michael Woolnough <michael.woolnough@gmail.com>
2016-04-23 08:41:09 +0100

    moved channel prop read to decoder

--- decoder.go
+++ decoder.go
@@ -236,7 +236,48 @@ func (d *Decoder) readChannel() channel {
 	width := d.r.ReadUint32()
 	height := d.r.ReadUint32()
 	name := d.r.ReadString()
-	d.readChannelProperties()
+	for {
+		propID := property(d.r.ReadUint32())
+		propLength := d.r.ReadUint32()
+		switch propID {
+		case propEnd:
+			return
+		case propActiveChannel:
+			//a := d.readActiveChannel()
+			// no data, just set as active
+		case propSelection:
+			//s := d.readSelection()
+			// no data, just set as selection
+		case propOpacity:
+			o := d.readOpacity()
+			_ = o
+		case propVisible:
+			v := d.readBool()
+			_ = v
+		case propLinked:
+			l := d.readBool()
+			_ = l
+		case propShowMasked:
+			s := d.readBool()
+			_ = s
+		case propColor:
+			r := d.r.ReadUint8()
+			g := d.r.ReadUint8()
+			b := d.r.ReadUint8()
+			_, _, _ = r, g, b
+		case propTattoo:
+			t := d.readTattoo()
+			_ = t
+		case propParasites:
+			p := d.readParasites(propLength)
+			_ = p
+		case propLockContent:
+			l := d.readBool()
+			_ = l
+		default:
+			d.s.Seek(int64(propLength), os.SEEK_CUR)
+		}
+	}
 	hptr := d.r.ReadUint32() //
 }
 
--- props.go
+++ props.go
@@ -1,9 +1,6 @@
 package xcf
 
-import (
-	"errors"
-	"os"
-)
+import "errors"
 
 type property uint8
 
@@ -43,51 +40,6 @@ const (
 	propLockPosition      property = 32
 	propFloatOpacity      property = 33
 )
-
-func (d *Decoder) readChannelProperties() {
-	for {
-		propID := property(d.r.ReadUint32())
-		propLength := d.r.ReadUint32()
-		switch propID {
-		case propEnd:
-			return
-		case propActiveChannel:
-			//a := d.readActiveChannel()
-			// no data, just set as active
-		case propSelection:
-			//s := d.readSelection()
-			// no data, just set as selection
-		case propOpacity:
-			o := d.readOpacity()
-			_ = o
-		case propVisible:
-			v := d.readBool()
-			_ = v
-		case propLinked:
-			l := d.readBool()
-			_ = l
-		case propShowMasked:
-			s := d.readBool()
-			_ = s
-		case propColor:
-			r := d.r.ReadUint8()
-			g := d.r.ReadUint8()
-			b := d.r.ReadUint8()
-			_, _, _ = r, g, b
-		case propTattoo:
-			t := d.readTattoo()
-			_ = t
-		case propParasites:
-			p := d.readParasites(propLength)
-			_ = p
-		case propLockContent:
-			l := d.readBool()
-			_ = l
-		default:
-			d.s.Seek(int64(propLength), os.SEEK_CUR)
-		}
-	}
-}
 
 func (d *Decoder) readBool() bool {
 	switch d.r.ReadUint32() {
