eabb9649e95d9d1273d6d3c5d1df6808783dab9a
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-05 13:48:22 +0100

    added property constants

---
+++ props.go
@@ -0,0 +1,38 @@
+package xcf
+
+const (
+	propEnd               = 0
+	propColormap          = 1
+	propActiveLayer       = 2
+	propActiveChannel     = 3
+	propSelection         = 4
+	propFloatingSelection = 5
+	propOpacity           = 6
+	propMode              = 7
+	propVisible           = 8
+	propLinked            = 9
+	propLockAlpha         = 10
+	propApplyMask         = 11
+	propEditMask          = 12
+	propShowMask          = 13
+	propShowMasked        = 14
+	propOffsets           = 15
+	propColor             = 16
+	propCompression       = 17
+	propGuides            = 18
+	propResolution        = 19
+	propTattoo            = 20
+	propParasites         = 21
+	propUnit              = 22
+	propPaths             = 23
+	propUserUnit          = 24
+	propVectors           = 25
+	propTextLayerFlags    = 26
+	propSamplePoints      = 27
+	propLockContent       = 28
+	propGroupItem         = 29
+	propItemPath          = 30
+	propGroupItemFlags    = 31
+	propLockPosition      = 32
+	propFloatOpacity      = 33
+)
