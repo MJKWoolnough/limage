39aa77df53a04dfd2021ed07e24789df2686cac2
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-05 16:52:04 +0100

    added paths reader

---
+++ paths.go
@@ -0,0 +1,77 @@
+package xcf
+
+import "errors"
+
+type paths struct {
+	aIndex uint32
+	paths  []path
+}
+
+type path struct {
+	name    string
+	linked  bool
+	state   byte
+	closed  bool
+	version uint32
+	tattoo  uint32
+	points  []pathPoint
+}
+
+type pathPoint struct {
+	control bool
+	x, y    float64
+}
+
+func (d *decoder) ReadPaths() paths {
+	var p paths
+	p.aIndex = d.ReadUint32()
+	n := d.ReadUint32()
+	p.paths = make([]path, n)
+	for i := uint32(0); i < n; i++ {
+		p.paths[i].name = d.ReadString()
+		p.paths[i].linked = d.ReadBoolProperty()
+		state := d.ReadUint8()
+		p.paths[i].closed = d.ReadBoolProperty()
+		if p.paths[i].closed {
+			if state != 4 {
+				d.Err = ErrInconsistantClosedState
+				return p
+			}
+		} else {
+			if state != 2 {
+				d.Err = ErrInconsistantClosedState
+				return p
+			}
+		}
+		np := d.ReadUint32()
+		v := d.ReadUint32()
+		if v < 1 || v > 3 {
+			d.Err = ErrUnknownPathsVersion
+			return p
+		}
+		if v == 2 || v == 3 {
+			d.ReadUint32()
+		}
+		if v == 3 {
+			p.paths[i].tattoo = d.ReadUint32()
+		}
+		p.paths[i].points = make([]pathPoint, np)
+		for j := uint32(0); j < np; j++ {
+			p.paths[i].points[j].control = d.ReadBoolProperty()
+			if v == 1 {
+				p.paths[i].points[j].x = float64(d.ReadInt32())
+				p.paths[i].points[j].z = float64(d.ReadInt32())
+			} else {
+				p.paths[i].points[j].x = float64(d.ReadFloat32())
+				p.paths[i].points[j].x = float64(d.ReadFloat32())
+			}
+		}
+	}
+	return p
+}
+
+// Errors
+var (
+	ErrInconsistantClosedState = errors.New("inconsistant closed state")
+	ErrUnknownPathsVersion     = errors.New("unknown paths version")
+)
