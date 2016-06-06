825cfa1c4801431d34a39c734c5fbbb78a63a91c
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-06 21:20:56 +0100

    initial layer reader

--- layer.go
+++ layer.go
@@ -2,6 +2,7 @@ package xcf
 
 import (
 	"errors"
+	"image"
 	"os"
 )
 
@@ -19,6 +20,19 @@ type layer struct {
 	mode                                        uint32
 	offsetX, offsetY                            int32
 	textLayerFlags                              uint32
+	image                                       image.Image
+	mask                                        struct {
+		name                         string
+		linked, lockContent, visible bool
+		opacity                      uint8
+		parasites                    parasites
+		tattoo                       uint32
+		active, selection, show      bool
+		color                        struct {
+			r, g, b uint8
+		}
+		image image.Image
+	}
 }
 
 func (d *decoder) ReadLayer() layer {
@@ -27,7 +41,7 @@ func (d *decoder) ReadLayer() layer {
 	l.height = d.ReadUint32()
 	typ := d.ReadUint()
 	if typ>>1 != d.baseType {
-		d.Err = ErrInvalidLayerType
+		d.SetError(ErrInvalidLayerType)
 		return l
 	}
 	l.name = d.ReadString()
@@ -41,7 +55,7 @@ PropertyLoop:
 		// general properties
 		case propEnd:
 			if plength != 0 {
-				d.Err = ErrInvalidProperties
+				d.SetError(ErrInvalidProperties)
 			}
 			break PropertyLoop
 		case propLinked:
@@ -51,9 +65,9 @@ PropertyLoop:
 		case propOpacity:
 			o := d.ReadUint32()
 			if o > 255 {
-				d.Err = ErrInvalidOpacity
+				d.SetError(ErrInvalidOpacity)
 			}
-			d.opacity = uint8(o)
+			l.opacity = uint8(o)
 		case propParasites:
 			l.parasites = d.ReadParasites()
 		case propTattoo:
@@ -74,7 +88,7 @@ PropertyLoop:
 			l.group = d.ReadBoolProperty()
 		case propItemPath:
 			if plength&3 != 0 {
-				d.Err = ErrInvalidItemPathLength
+				d.SetError(ErrInvalidItemPathLength)
 			}
 			l.itemPath = make([]uint32, plength>>2)
 			for i := uint32(0); i < plength>>2; i++ {
@@ -97,10 +111,80 @@ PropertyLoop:
 			d.Seek(int64(plength), os.SEEK_CUR)
 		}
 	}
+
+	hptr := d.ReadUint32()
+	mptr := d.ReadUint32()
+
+	d.Seek(int64(hptr))
+	// read hierarchy
+
+	l.image = d.ReadImage(l.width, l.height, l.mode)
+
+	if mptr != 0 { // read layer mask
+		d.Seek(int64(mptr))
+		width := d.ReadUint32()
+		height := d.ReadUint32()
+		if width != l.width || height != l.height {
+			d.SetError(ErrInconsistantData)
+			return l
+		}
+		l.mask.name = d.ReadString()
+
+		for {
+			typ := d.ReadUint32()
+			plength := d.ReadUint32()
+			switch typ {
+			// general properties
+			case propEnd:
+				if plength != 0 {
+					d.SetError(ErrInvalidProperties)
+				}
+				break PropertyLoop
+			case propLinked:
+				l.mask.linked = d.ReadBoolProperty()
+			case propLockContent:
+				l.mask.lockContent = d.ReadBoolProperty()
+			case propOpacity:
+				o := d.ReadUint32()
+				if o > 255 {
+					d.SetError(ErrInvalidOpacity)
+				}
+				l.mask.opacity = uint8(o)
+			case propParasites:
+				l.mask.parasites = d.ReadParasites()
+			case propTattoo:
+				l.mask.tattoo = d.ReadUint32()
+			case propVisible:
+				l.mask.visible = d.ReadBoolProperty()
+
+				//mask properties
+			case propActiveChannel:
+				l.mask.active = true
+			case propColor:
+				l.mask.color.r = d.ReadUint8()
+				l.mask.color.g = d.ReadUint8()
+				l.mask.color.b = d.ReadUint8()
+			case propSelection:
+				l.mask.selection = true
+			case propShowMasked:
+				l.mask.show = d.ReadBoolProperty()
+			default:
+				d.Seek(int64(plength), os.SEEK_CUR)
+			}
+
+		}
+
+		hptr := d.ReadUint32()
+		d.Seek(int64(hptr))
+
+		l.mask.image = d.ReadImage(l.width, l.height, l.mode) // gray???
+	}
+	return l
 }
 
 // Errors
 var (
 	ErrInvalidLayerType      = errors.New("invalid layer type")
 	ErrInvalidItemPathLength = errors.New("invalid item path length")
+	ErrInconsistantData      = errors.New("inconsistant data read")
 )
