38046f63dafec383c918cedbe54632a7abc569b1
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-07 12:29:12 +0100

    initial rgb reader

--- colours.go
+++ colours.go
@@ -11,6 +11,100 @@ type colourReader interface {
 	io.Seeker
 }
 
+type rgb struct {
+	R, G, B uint8
+}
+
+func (rgb rgb) RGBA() (r, g, b, a uint32) {
+	r = rgb.R
+	r |= r << 8
+	g = rgb.G
+	g |= g << 8
+	b = rgb.B
+	b |= b << 8
+	return r, g, b, 0xFFFF
+}
+
+func rgbColourModel(c color.Color) color.Color {
+	r, g, b, _ := c.RGBA()
+	return rgb{
+		R: r >> 8,
+		G: g >> 8,
+		B: b >> 8,
+	}
+}
+
+type rgbImage struct {
+	Pix    []rgb
+	Stride int
+	Rect   image.Rectangle
+}
+
+func (r *rgbImage) At(x, y int) color.Color {
+	return r.RGBAt(x, y)
+}
+
+func (r *rgbImage) ColorModel() color.Model {
+	return rgbColourModel
+}
+
+func (r *rgbImage) RGBAt(x, y int) rgb {
+	if !(image.Point{x, y}.In(r.Rect)) {
+		return rgb{}
+	}
+	return r.Pix[r.PixOffset(x, y)]
+}
+
+func (r *rgbImage) Opaque() bool {
+	return true
+}
+
+func (r *rgbImage) PixOffset(x, y int) int {
+	return (y-r.Rect.Min.Y)*r.Stride + x - r.Rect.Min.X
+}
+
+func (rgb *rgbImage) Set(x, y int, c color.Color) {
+	if !(image.Point{x, y}.In(rgb.Rect)) {
+		return
+	}
+	r, g, b, _ := c.RGBA()
+	rgb.Pix[r.PixOffset(x, y)] = rgb{
+		R: r >> 8,
+		G: g >> 8,
+		B: b >> 8,
+	}
+}
+
+func (r *rgbImage) SetRGB(x, y int, rgb rgb) {
+	if !(image.Point{x, y}.In(r.Rect)) {
+		return
+	}
+	r.Pix[r.PixOffset(x, y)] = rgb
+}
+
+func (rgb *rgbImage) SubImage(r image.Rectangle) image.Image {
+	r = r.Intersect(rgb.Rect)
+	if r.Empty() {
+		return &rgbImage{}
+	}
+	return &rgbImage{
+		Pix:    rgb.Pix[rgb.PixOffset(r.Min.X, r.Min.Y)],
+		Stride: rgb.Stride,
+		Rect:   r,
+	}
+}
+
+type rgbImageReader struct {
+	*rgb
+}
+
+func (r rgbImageReader) ReadColour(x, y uint32, cr colourReader) {
+	r := cr.ReadByte()
+	g := cr.ReadByte()
+	b := cr.ReadByte()
+	r.SetRGB(x, y, rgb{r, g, b})
+}
+
 type grayAlpha struct {
 	Y, A uint8
 }
@@ -28,7 +122,7 @@ type grayAlphaImage struct {
 	Alpha []uint8
 }
 
-func newGrayAlpha(r image.Rect) grayAlphaImage {
+func newGrayAlpha(r image.Rect) *grayAlphaImage {
 
 }
 
@@ -54,4 +148,38 @@ func (g *grayAlphaImage) SetGrayAlpha(x, y int, ga grayAlpha) {
 
 func (g *grayAlphaImage) SubImage(r image.Rectangle) image.Image {
 
+}
+
+type palettedAlpha struct {
+	image.Paletted
+	Alpha []uint8
+}
+
+type indexedAlpha struct {
+	I, A uint8
+}
+
+func newPalettedAlpha(r image.Rect, p color.Palette) *palettedAlpha {
+
+}
+
+func (p *palettedAlpha) At(x, y int) color.Color {
+
+}
+
+func (g *palettedAlpha) ColorModel() {
+
+}
+
+func (g *palettedAlpha) IndexAlphaAt(x, y int) indexedAlpha {
+
+}
+
+func (g *palettedAlpha) Opaque() {
+}
+
+func (g *palettedAlpha) SetIndexAlpha(x, y int, ia indexedAlpha) {
+}
+
+func (g *palettedAlpha) SubImage(r image.Rectangle) image.Image {
 }
