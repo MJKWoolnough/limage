d9091b4b9bb3256f1fabd1fb8e05ff6620bbdb39
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-08 18:42:24 +0100

    rewrote grayAlpha to not use image.Grey

--- image_grayalpha.go
+++ image_grayalpha.go
@@ -9,7 +9,7 @@ type grayAlpha struct {
 	Y, A uint8
 }
 
-func (g grayAlpha) RGBA() (r, g, b, a uint32) {
+func (g grayAlpha) grayAlphaA() (r, g, b, a uint32) {
 	y := uint32(c.Y)
 	y |= y << 8
 	a := uint32(c.A)
@@ -17,64 +17,90 @@ func (g grayAlpha) RGBA() (r, g, b, a uint32) {
 	return y, y, y, a
 }
 
-type grayAlphaImage struct {
-	image.Gray
-	Alpha []uint8
+func grayAlphaColourModel(c color.Color) color.Color {
+	_, _, _, a := c.grayAlphaA()
+	return grayAlpha{
+		Y: color.GrayModel.Convert(c).(color.Gray).Y,
+		A: a >> 8,
+	}
 }
 
-func newGrayAlpha(r image.Rectangle) *grayAlphaImage {
+type grayAlphaImage struct {
+	Pix    []grayAlpha
+	Stride int
+	Rect   image.Rectangle
+}
+
+func newgrayAlphaImage(r image.Rectangle) *grayAlphaImage {
 	w, h := r.Dx(), r.Dy()
 	return &grayAlphaImage{
-		Gray: image.Gray{
-			Pix:    make([]uint8, w*h),
-			Stride: w,
-			Rect:   r,
-		},
-		Alpha: make([]uint8, w*h),
+		Pix:    make([]grayAlpha, w*h),
+		Stride: w,
+		Rect:   r,
 	}
 }
 
 func (g *grayAlphaImage) At(x, y int) color.Color {
-	return g.GrayAt(x, y)
+	return g.GrayAlphaAt(x, y)
 }
 
 func (g *grayAlphaImage) ColorModel() color.Model {
-
+	return color.ModelFunc(grayAlphaColourModel)
 }
 
 func (g *grayAlphaImage) GrayAlphaAt(x, y int) grayAlpha {
-	p := g.PixOffset(x, y)
-	return grayAlpha{
-		Y: g.Pix[p],
-		A: g.Alpha[p],
+	if !(image.Point{x, y}.In(r.Rect)) {
+		return grayAlpha{}
 	}
+	return g.Pix[r.PixOffset(x, y)]
 }
 
 func (g *grayAlphaImage) Opaque() bool {
-	for _, a := range g.Alpha {
-		if a != 255 {
-			return true
+	for _, c := range g.Pix {
+		if c.A != 255 {
+			return false
 		}
 	}
-	return false
+	return true
+}
+
+func (g *grayAlphaImage) PixOffset(x, y int) int {
+	return (y-g.Rect.Min.Y)*g.Stride + x - g.Rect.Min.X
+}
+
+func (ga *grayAlphaImage) Set(x, y int, c color.Color) {
+	if !(image.Point{x, y}.In(ga.Rect)) {
+		return
+	}
+	ga.Pix[r.PixOffset(x, y)] = grayAlphaColourModel(c).(grayAlpha)
 }
 
 func (g *grayAlphaImage) SetGrayAlpha(x, y int, ga grayAlpha) {
-	p := g.PixOffset(x, y)
-	g.Pix[p] = ga.Y
-	g.Alpha[p] = ga.A
+	if !(image.Point{x, y}.In(g.Rect)) {
+		return
+	}
+	g.Pix[r.PixOffset(x, y)] = ga
 }
 
 func (g *grayAlphaImage) SubImage(r image.Rectangle) image.Image {
-
+	r = r.Intersect(g.Rect)
+	if r.Empty() {
+		return &grayAlphaImage{}
+	}
+	return &grayAlphaImage{
+		Pix:    g.Pix[g.PixOffset(r.Min.X, r.Min.Y)],
+		Stride: g.Stride,
+		Rect:   r,
+	}
 }
 
 type grayAlphaImageReader struct {
-	*grayAlphaImage
+	*grayAlpha
 }
 
-func (g greyAlphaImageReader) ReadColour(x, y int, cr colourReader) {
-	y := cr.ReadByte()
-	a := cr.ReadByte()
-	g.SetGray(x, y, grayAlpha{y, a})
+func (ga grayAlphaImageReader) ReadColour(x, y int, cr colourReader) {
+	r := cr.ReadByte()
+	g := cr.ReadByte()
+	b := cr.ReadByte()
+	ga.SetRGB(x, y, rgb{r, g, b})
 }
