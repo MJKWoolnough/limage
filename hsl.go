d958cf06dc8e1afc3c38a05b876c8a00d5fa9603
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-26 14:44:25 +0100

    start of hsv/hsl code

---
+++ hsl.go
@@ -0,0 +1,27 @@
+package xcf
+
+import "image/color"
+
+type hsl struct {
+	H, S, L, A uint16
+}
+
+func rgbToHSL(cl color.Color) hsl {
+	return hsv{}
+}
+
+func (h hsl) RGBA() (uint32, uint32, uint32, uint32) {
+	return 0, 0, 0, 0
+}
+
+type hsv struct {
+	H, S, V, A uint16
+}
+
+func rgbToHSV(cl color.Color) hsv {
+	return hsv{}
+}
+
+func (h hsv) RGBA() (uint32, uint32, uint32, uint32) {
+	return 0, 0, 0, 0
+}
