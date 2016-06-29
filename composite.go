2000cbc003fbe0829ed63507e8133c46dc86c5b7
Michael Woolnough <michael.woolnough@gmail.com>
2016-06-29 13:05:23 +0100

    colours needs to not be alpha-premultiplied for composition to work

--- composite.go
+++ composite.go
@@ -81,8 +81,8 @@ func (c Composite) String() string {
 	}
 }
 
-func (c Composite) Composite(bottom, top color.Color) color.Color {
-	var f func(uint32, uint32) uint32
+func (c Composite) Composite(bottom, top color.NRGBA64) color.Color {
+	var f func(uint16, uint16) uint16
 	switch c {
 	case CompositeDissolve:
 		return compositeDissolve(bottom, top)
@@ -129,138 +129,147 @@ func (c Composite) Composite(bottom, top color.Color) color.Color {
 	default: //Normal
 		return compositeNormal(bottom, top)
 	}
-	ar, ag, ab, aa := bottom.RGBA()
-	if aa == 0 {
-		return color.Alpha{}
+	if bottom.A == 0 {
+		return color.NRGBA{}
 	}
-	br, bg, bb, ba := top.RGBA()
-	return color.RGBA64{
-		R: uint16(blend(aa, ar, min(aa, ba), f(ar, br))),
-		G: uint16(blend(aa, ag, min(aa, ba), f(ag, bg))),
-		B: uint16(blend(aa, ab, min(aa, ba), f(ab, bb))),
-		A: uint16(aa),
-	}
-}
-
-func compositeNormal(bottom, top color.Color) color.Color {
-	ar, ag, ab, aa := bottom.RGBA()
-	br, bg, bb, ba := top.RGBA()
-	if aa == 0 && ba == 0 {
-		return color.Alpha{}
-	}
-	return color.RGBA64{
-		R: uint16(blend(aa, ar, ba, br)),
-		G: uint16(blend(aa, ag, ba, bg)),
-		B: uint16(blend(aa, ab, ba, bb)),
-		A: uint16(0xffff - (0xffff-aa)*(0xffff-ba)/0xffff),
+	ma := min(bottom.A, top.A)
+	return color.NRGBA64{
+		R: blend(bottom.A, bottom.R, ma, f(bottom.R, top.R)),
+		G: blend(bottom.A, bottom.G, ma, f(bottom.R, top.G)),
+		B: blend(bottom.A, bottom.B, ma, f(bottom.R, top.B)),
+		A: bottom.A,
 	}
 }
 
-func compositeDissolve(bottom, top color.Color) color.Color {
-	r, g, b, a := top.RGBA()
-	if uint32(rand.Int31n(0xffff)) < a {
-		return color.RGBA64{
-			R: uint16(r),
-			G: uint16(g),
-			B: uint16(b),
-			A: 0xffff,
+func compositeNormal(bottom, top color.NRGBA64) color.NRGBA64 {
+	/*
+		ar, ag, ab, aa := bottom.RGBA()
+		br, bg, bb, ba := top.RGBA()
+		if aa == 0 && ba == 0 {
+			return color.Alpha{}
 		}
+		return color.RGBA64{
+			R: uint16(blend(aa, ar, ba, br)),
+			G: uint16(blend(aa, ag, ba, bg)),
+			B: uint16(blend(aa, ab, ba, bb)),
+			A: uint16(0xffff - (0xffff-aa)*(0xffff-ba)/0xffff),
+		}
+	*/
+	if bottom.A == 0 && top.A == 0 {
+		return color.NRGBA64{}
+	}
+	return color.NRGBA64{
+		R: blend(bottom.A, bottom.R, top.A, top.R),
+		G: blend(bottom.A, bottom.G, top.A, top.G),
+		B: blend(bottom.A, bottom.B, top.A, top.B),
+		A: uint16(0xffff - (0xffff-uint32(bottom.A))*(0xffff-uint32(top.A))/0xffff),
+	}
+}
+
+func compositeDissolve(bottom, top color.NRGBA64) color.NRGBA64 {
+	if uint16(rand.Int31n(0xffff)) < bottom.A {
+		top.A = 0xffff
+		return top
 	}
 	return bottom
 }
 
-func compositeMultiply(x, y uint32) uint32 {
-	return x * y / 0xffff
+func compositeMultiply(x, y uint16) uint16 {
+	return uint16(uint32(x) * uint32(y) / 0xffff)
 }
 
-func compositeScreen(x, y uint32) uint32 {
-	return 0xffff - (0xffff-x)*(0xffff-y)/0xffff
+func compositeScreen(x, y uint16) uint16 {
+	return uint16(0xffff - uint32(0xffff-x)*uint32(0xffff-y)/0xffff)
 }
 
-func compositeOverlay(x, y uint32) uint32 {
+func compositeOverlay(ax, ay uint16) uint16 {
+	x := uint32(ax)
+	y := uint32(ay)
 	t := 0xffff - y
-	return (0xffff-y)*(x*x/0xffff)/0xffff + y*(0xffff-(t*t/0xffff))/0xffff
+	return uint16((0xffff-y)*(x*x/0xffff)/0xffff + y*(0xffff-(t*t/0xffff))/0xffff)
 }
 
-func compositeDifference(x, y uint32) uint32 {
+func compositeDifference(x, y uint16) uint16 {
 	if x > y {
 		return x - y
 	}
 	return y - x
 }
 
-func compositeAddition(x, y uint32) uint32 {
-	return clamp(x + y)
+func compositeAddition(x, y uint16) uint16 {
+	return clamp(uint32(x) + uint32(y))
 }
 
-func compositeSubtract(x, y uint32) uint32 {
+func compositeSubtract(x, y uint16) uint16 {
 	if y > x {
 		return 0
 	}
 	return x - y
 }
 
-func compositeDarkenOnly(x, y uint32) uint32 {
+func compositeDarkenOnly(x, y uint16) uint16 {
 	return min(x, y)
 }
 
-func compositeLightenOnly(x, y uint32) uint32 {
+func compositeLightenOnly(x, y uint16) uint16 {
 	return max(x, y)
 }
 
-func compositeDivide(x, y uint32) uint32 {
+func compositeDivide(x, y uint16) uint16 {
 	if y == 0 {
 		if x == 0 {
 			return 0
 		}
 		return 0xffff
 	}
-	return clamp(0xffff * x / y)
+	return clamp(0xffff * uint32(x) / uint32(y))
 }
 
-func compositeDodge(x, y uint32) uint32 {
+func compositeDodge(x, y uint16) uint16 {
 	if y == 0xffff {
 		if x == 0 {
 			return 0
 		}
 		return 0xffff
 	}
-	return clamp(0xffff * x / (0xffff - y))
+	return clamp(0xffff * uint32(x) / (0xffff - uint32(y)))
 }
 
-func compositeBurn(x, y uint32) uint32 {
+func compositeBurn(x, y uint16) uint16 {
 	if y == 0 {
 		if x == 0xffff {
 			return 0
 		}
 		return 0xffff
 	}
-	return clamp(0xffff - 0xffff*(0xffff-x)/y)
+	return clamp(0xffff - 0xffff*(0xffff-uint32(x))/uint32(y))
 }
 
-func compositeHardLight(x, y uint32) uint32 {
+func compositeHardLight(x, y uint16) uint16 {
 	if y < 0x7fff {
-		return x * (y << 1) / 0xffff
+		return uint16(uint32(x) * (uint32(y) << 1) / 0xffff)
 	}
-	return 0xffff - (0xffff-x)*(0x1fffe-y<<1)/0xffff
+	return uint16(0xffff - (0xffff-uint32(x))*(0x1fffe-uint32(y)<<1)/0xffff)
 }
 
-func compositeSoftLight(x, y uint32) uint32 {
+func compositeSoftLight(x, y uint16) uint16 {
 	return compositeOverlay(x, y)
 }
 
-func compositeGrainExtract(x, y uint32) uint32 {
+func compositeGrainExtract(ax, ay uint16) uint16 {
+	x := uint32(ax)
+	y := uint32(ay)
 	if x+0x7fff < y {
 		return 0
 	}
 	return clamp(x - y + 0x7fff)
 }
 
-func compositeGrainMerge(x, y uint32) uint32 {
-	return clamp(x + y + 0x7fff)
+func compositeGrainMerge(x, y uint16) uint16 {
+	return clamp(uint32(x) + uint32(y) + 0x7fff)
 }
 
-func compositeHue(bottom, top color.Color) color.Color {
+func compositeHue(bottom, top color.NRGBA64) color.Color {
 	br, bg, bb, _ := top.RGBA()
 	if br == bg && br == bb {
 		return bottom
@@ -271,29 +280,29 @@ func compositeHue(bottom, top color.Color) color.Color {
 	return a
 }
 
-func compositeSaturation(bottom, top color.Color) color.Color {
+func compositeSaturation(bottom, top color.NRGBA64) color.Color {
 	a := rgbToHSV(bottom)
 	b := rgbToHSV(top)
 	a.S = b.S
 	return a
 }
 
-func compositeColor(bottom, top color.Color) color.Color {
+func compositeColor(bottom, top color.NRGBA64) color.Color {
 	a := rgbToHSL(bottom)
 	b := rgbToHSL(top)
 	b.L = a.L
 	return b
 }
 
-func compositeValue(bottom, top color.Color) color.Color {
+func compositeValue(bottom, top color.NRGBA64) color.Color {
 	a := rgbToHSV(bottom)
 	b := rgbToHSV(top)
 	a.V = b.V
 	return a
 }
 
-func min(n ...uint32) uint32 {
-	var m uint32 = 0xffffffff
+func min(n ...uint16) uint16 {
+	var m uint16 = 0xffff
 	for _, o := range n {
 		if o < m {
 			m = o
@@ -302,8 +311,8 @@ func min(n ...uint32) uint32 {
 	return m
 }
 
-func max(n ...uint32) uint32 {
-	var m uint32
+func max(n ...uint16) uint16 {
+	var m uint16
 	for _, o := range n {
 		if o > m {
 			m = o
@@ -312,18 +321,22 @@ func max(n ...uint32) uint32 {
 	return m
 }
 
-func mid(n ...uint32) uint32 {
+func mid(n ...uint16) uint16 {
 	return (min(n...) + max(n...)) >> 1
 }
 
-func clamp(n uint32) uint32 {
+func clamp(n uint32) uint16 {
 	if n > 0xffff {
 		return 0xffff
 	}
-	return n
+	return uint16(n)
 }
 
-func blend(a1, x1, a2, x2 uint32) uint32 {
+func blend(aa1, ax1, aa2, ax2 uint16) uint16 {
+	a1 := uint32(aa1)
+	x1 := uint32(ax1)
+	a2 := uint32(aa2)
+	x2 := uint32(ax2)
 	k := 0xffff * a2 / (0xffff - (0xffff-a1)*(0xffff-a2)/0xffff)
-	return (0xffff-k)*x1/0xffff + k*x2/0xffff
+	return uint16((0xffff-k)*x1/0xffff + k*x2/0xffff)
 }
