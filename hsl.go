0e9d0af7fba04f4bb3dbbc78cc80d9021173f908
Michael Woolnough <michael.woolnough@gmail.com>
2016-07-02 15:24:29 +0100

    wrote rgbToHSL

--- hsl.go
+++ hsl.go
@@ -2,26 +2,74 @@ package xcf
 
 import "image/color"
 
-type hsl struct {
+type HSLA struct {
 	H, S, L, A uint16
 }
 
-func rgbToHSL(cl color.Color) hsl {
-	return hsl{}
+func rgbToHSL(cl color.NRGBA64) HSLA {
+	mn := uint32(min(cl.R, cl.G, cl.B))
+	mx := uint32(max(cl.R, c.G, cl.B))
+	l := mx + mn
+	hsl := HSLA{
+		L: uint16(l >> 1),
+		A: cl.A,
+	}
+	c := max - min
+	if c == 0 {
+		return hsl
+	}
+	if l <= 0xffff {
+		hsl.S = uint16(0xffff * c / l)
+	} else {
+		hsl.S = uint16(0xffff * c / (0x1fffe - l))
+	}
+	var h uint32
+	switch uint16(mx) {
+	case cl.R:
+		if cl.G > cl.B {
+			h = 0xffff * (cl.G - cl.B) / c
+		} else {
+			h = 0x5fffa - 0xffff*(cl.B-cl.G)/c
+		}
+	case cl.G:
+		if cl.B > cl.R {
+			h = 0x1fffe + 0xffff*(cl.B-cl.R)/c
+		} else {
+			h = 0x1fffe - 0xffff*(cl.R-cl.B)/c
+		}
+	case cl.B:
+		if cl.R > cl.G {
+			h = 0x3fffc + 0xffff*(cl.R-cl.G)/c
+		} else {
+			h = 0x3fffc - 0xffff*(cl.G-cl.R)/c
+		}
+	}
+
+	hsl.H = h / 6
+
+	return hsl
 }
 
-func (h hsl) RGBA() (uint32, uint32, uint32, uint32) {
-	return 0, 0, 0, 0
+func (h HSLA) RGBA() (uint32, uint32, uint32, uint32) {
+	return h.toNRGBA().RGBA()
 }
 
-type hsv struct {
+func (h HSLA) toNRGBA() color.NRGBA64 {
+	return color.NRGBA64{}
+}
+
+type HSVA struct {
 	H, S, V, A uint16
 }
 
-func rgbToHSV(cl color.Color) hsv {
-	return hsv{}
+func rgbToHSV(cl color.NRGBA64) HSVA {
+	return HSVA{}
 }
 
-func (h hsv) RGBA() (uint32, uint32, uint32, uint32) {
-	return 0, 0, 0, 0
+func (h HSVA) RGBA() (uint32, uint32, uint32, uint32) {
+	return h.toNRGBA().RGBA()
+}
+
+func (h HSVA) toNRGBA() color.NRGBA64 {
+	return color.NRGBA64{}
 }
