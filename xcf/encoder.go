96b3626151496f61730a672a0ff0386d666736e7
Michael Woolnough <michael.woolnough@gmail.com>
2016-09-13 22:23:48 +0100

    added headerWriter

--- xcf/encoder.go
+++ xcf/encoder.go
@@ -1,15 +1,24 @@
 package xcf
 
 import (
-	"errors"
 	"image"
+	"image/color"
 	"io"
 
 	"github.com/MJKWoolnough/limage"
+	"github.com/MJKWoolnough/limage/lcolor"
 )
+
+type encoder struct {
+	writer
+	channels map[string]uint64
+
+	colorModel color.Model
+}
 
 func Encode(w io.WriterAt, i image.Image) error {
 	var im *limage.Image
+
 	if m, ok := i.(*limage.Image); ok {
 		im = m
 	} else {
@@ -32,6 +41,40 @@ func Encode(w io.WriterAt, i image.Image) error {
 			Opacity: 255,
 		}
 	}
-	_ = im
-	return errors.New("unimplemented")
+
+	e := encoder{
+		writer:   newWriter(w),
+		channels: make(map[string]uint64),
+	}
+
+	e.WriteHeader(im.Config)
+
+	// write property list
+
+	e.WriteUint32(0)
+
+	// write channel list
+
+	e.WriteUint32(0)
+
+	return e.Err
+}
+
+var header = []byte{'g', 'i', 'm', 'p', ' ', 'x', 'c', 'f', 'v', '0', '0', '3', '0'}
+
+func (e *encoder) WriteHeader(c *image.Config) {
+	e.Write(header)
+	e.WriteUint32(uint32(c.Width))
+	e.WriteUint32(uint32(c.Height))
+	switch c.ColorModel {
+	case color.GrayModel, color.Gray16Model, lcolor.GrayAlphaModel:
+		e.colorModel = lcolor.GrayAlphaModel
+		e.WriteUint32(1)
+	case color.Palette, lcolor.AlphaPalette:
+		e.colorModel = c.ColorModel
+		e.WriteUint32(2)
+	default:
+		e.colorModel = color.RGBAModel
+		e.WriteUint32(0)
+	}
 }
