5e6e6db3003f5450ea014018520115b99d5a54ab
Michael Woolnough <michael.woolnough@gmail.com>
2016-12-09 13:06:13 +0000

    removed early return

--- ora/stack.go
+++ ora/stack.go
@@ -122,7 +122,9 @@ func (d decoder) readLayer(s *xml.StartElement, offset image.Point) (limage.Laye
 	if s.Name.Local == "stack" {
 		var err error
 		l.Image, err = d.readStack(offset.Add(l.LayerBounds.Min))
-		return l, err
+		if err != nil {
+			return nil, err
+		}
 	} else {
 		for _, f := range d.zr.File {
 			if f.Name == source {
