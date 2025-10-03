d08ddf2fd2c75120de1fcc53ae8242b6cea1bf70
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-03 07:22:56 +0100

    cleaned up code and made more readable

--- xcf/channel.go
+++ xcf/channel.go
@@ -7,55 +7,7 @@ func (d *decoder) ReadChannel() *image.Gray {
 	height := d.ReadUint32()
 
 	d.SkipString() // name
-
-PropertyLoop:
-	for {
-		typ := d.ReadUint32()
-		plength := d.ReadUint32()
-
-		switch typ {
-		// general properties
-		case propEnd:
-			if plength != 0 {
-				d.SetError(ErrInvalidProperties)
-			}
-
-			break PropertyLoop
-		case propLinked:
-			d.SkipBoolProperty()
-		case propLockContent:
-			d.SkipBoolProperty()
-		case propOpacity:
-			if d.ReadUint32() > 255 {
-				d.SetError(ErrInvalidOpacity)
-			}
-		case propParasites:
-			d.SkipParasites(plength)
-		case propTattoo:
-			d.SkipUint32()
-		case propVisible:
-			d.SkipBoolProperty()
-
-			// channel properties
-		case propActiveChannel:
-			// active channel
-		case propColor:
-			d.SkipByte() // r
-			d.SkipByte() // g
-			d.SkipByte() // b
-		case propFloatColour:
-			d.SkipFloat32() // r
-			d.SkipFloat32() // g
-			d.SkipFloat32() // b
-		case propSelection:
-			// selected
-		case propShowMasked:
-			d.SkipBoolProperty()
-		default:
-			d.Skip(plength)
-		}
-
-	}
+	d.skipProperties()
 
 	var hptr uint64
 
@@ -72,6 +24,30 @@ PropertyLoop:
 	}
 
 	return nil
+}
+
+func (d *decoder) skipProperties() {
+	for {
+		typ := d.ReadUint32()
+		plength := d.ReadUint32()
+
+		switch typ {
+		// general properties
+		case propEnd:
+			if plength != 0 {
+				d.SetError(ErrInvalidProperties)
+			}
+
+			return
+		case propOpacity:
+			if d.ReadUint32() > 255 {
+				d.SetError(ErrInvalidOpacity)
+			}
+		default:
+			d.Skip(plength)
+		}
+
+	}
 }
 
 func (e *encoder) WriteChannel(c *image.Gray) {
